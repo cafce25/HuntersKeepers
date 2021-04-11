2619755772f5fec7ce18aead62d6da7989793c76
Jonathan Birk <jona@jbirk.de>
2021-04-11 19:58:46 +0000

    Fix stray backslashes in professionals Mobility move

--- db/seeds/playbooks/professional.seeds.rb
+++ db/seeds/playbooks/professional.seeds.rb
@@ -70,9 +70,10 @@ after :playbook do
       name: 'Mobility',
       type: 'Moves::Descriptive',
       playbook_id: @professional.id,
-      description: 'You have a truck, van, or car built for \      monster hunting. Choose two good things and one
-      bad thing about it. \
-      Good things: roomy; surveillance gear; fast; \
+      description: 'You have a truck, van, or car built for
+      monster hunting. Choose two good things and one
+      bad thing about it.
+      Good things: roomy; surveillance gear; fast;
       stealthy; intimidating; classic; medical kit; sleeping space; toolkit; concealed weapons; anonymous;
       armoured (+1 armour inside); tough; monster cage.
       Bad things: loud; obvious; temperamental; beaten-up; gas-guzzler; uncomfortable; slow; old.'
